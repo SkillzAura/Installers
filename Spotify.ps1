e46cdd1179bee9e5fbd841b90bcad09567ff2949
Aura <131694696+SkillzAura@users.noreply.github.com>
2025-03-13 16:38:00 +0600

    Update Spotify.ps1

--- Spotify.ps1
+++ Spotify.ps1
@@ -1,6 +1,7 @@
-If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) 
-{	Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
-    Exit	}
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrator")) {
+    Start-Process powershell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"& { iwr -useb 'https://raw.githubusercontent.com/SkillzAura/Installers/main/Spotify.ps1' | iex }`""
+    return
+}
 
 iex "& { $(iwr -useb 'https://raw.githubusercontent.com/SpotX-Official/spotx-official.github.io/main/run.ps1') } -confirm_uninstall_ms_spoti -confirm_spoti_recomended_uninstall -podcasts_off -adsections_off -block_update_on -lyrics_stat purple -old_lyrics -cache_limit 500 -no_shortcut -DisableStartup"
 
