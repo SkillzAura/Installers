6efde68f6ece2b1549c3385063cddb02bf18cec3
Aura <131694696+SkillzAura@users.noreply.github.com>
2025-03-13 16:52:08 +0600

    Update Spotify.ps1

--- Spotify.ps1
+++ Spotify.ps1
@@ -3,7 +3,7 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     return
 }
 
-iex "& { $(iwr -useb 'https://raw.githubusercontent.com/SpotX-Official/spotx-official.github.io/main/run.ps1') } -confirm_uninstall_ms_spoti -confirm_spoti_recomended_uninstall -podcasts_off -adsections_off -block_update_on -lyrics_stat purple -old_lyrics -cache_limit 500 -no_shortcut -DisableStartup"
+iex "& { $(iwr -useb 'https://raw.githubusercontent.com/SkillzAura/Installers/refs/heads/main/SpotX.ps1') } -confirm_uninstall_ms_spoti -confirm_spoti_recomended_uninstall -podcasts_off -adsections_off -block_update_on -lyrics_stat purple -old_lyrics -cache_limit 500 -no_shortcut -DisableStartup"
 
 Remove-Item "C:\Users\$env:username\AppData\Roaming\Spotify\Spotify.bak"
 Remove-Item "C:\Users\$env:username\AppData\Roaming\Spotify\locales\*" -Exclude en-US.pak
